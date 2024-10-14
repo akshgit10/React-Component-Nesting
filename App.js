d8f0bb8705de1eea4fdf2c2cf482d7d361f28eec
Akshitha G <137154397+akshgit10@users.noreply.github.com>
2024-10-14 12:15:12 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Parent from './components/parent/Parent';
+function App() {
+  return (
+    <div className="App">
+      <br></br>
+      <Parent />
+    </div>
+  );
+}
+
+export default App;
